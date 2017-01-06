405a244e90340a08503c33f674c7f618496dc4a3
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-01-06 16:22:40 +0000

    Step 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,7 +9,7 @@ class DockingStation
   end
 
   def release_bike
-    if @bikes.empty?
+    if empty? 
        raise "No bikes available"
     else
        @bikes.pop
@@ -17,12 +17,26 @@ class DockingStation
   end
 
   def dock(bike)
-     fail "Maximum capacity is reached" if @bikes.count >= 20
+    if full?
+     fail "Maximum capacity is reached"
+    else
      @bikes  << bike
+   end
   end
+
 
   def show_bike
     @bikes
+  end
+
+  private
+
+  def full?
+     @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
