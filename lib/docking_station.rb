ce30f06589037c3e1670b58a745f2191dfa99224
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-01-06 16:46:01 +0000

    Step 16

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,7 @@
 require_relative 'bike'
 
+DEFAULT_CAPACITY = 20
+
 class DockingStation
 
   attr_reader  :bike
@@ -9,7 +11,7 @@ class DockingStation
   end
 
   def release_bike
-    if empty? 
+    if empty?
        raise "No bikes available"
     else
        @bikes.pop
@@ -32,7 +34,7 @@ class DockingStation
   private
 
   def full?
-     @bikes.count >= 20
+     @bikes.count >= DEFAULT_CAPACITY
   end
 
   def empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -15,7 +15,7 @@ end
     expect {subject.release_bike}.to raise_error "No bikes available"
   end
     it "raises an error if maximum capacity has been reached" do
-    expect {21.times {subject.dock Bike.new}}.to raise_error "Maximum capacity is reached"
+    expect {(DockingStation::DEFAULT_CAPACITY + 1).times {subject.dock Bike.new}}.to raise_error "Maximum capacity is reached"
     end
 
 
